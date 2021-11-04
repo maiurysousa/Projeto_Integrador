6ba69c18adb5406f8eadc1f84563e342e7a659d3
glaubertorres <glauberthierryt@gmail.com>
2021-11-04 11:32:12 -0300

    adicionando pasta documentação

--- Documentação/db_marketplace_mysql_create.sql
+++ Documentação/db_marketplace_mysql_create.sql
@@ -1,29 +1,27 @@
-CREATE DATABASE db_marketplace;
-USE db_marketplace;
-
 CREATE TABLE `tb_usuario` (
 	`id` bigint NOT NULL AUTO_INCREMENT,
 	`nome_completo` varchar(255) NOT NULL,
-	`usuario` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
 	`senha` varchar(255) NOT NULL,
+	`descricao` varchar(25) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `tb_categoria` (
 	`id` bigint NOT NULL AUTO_INCREMENT,
-	`categoria` varchar(255) NOT NULL,
-	`descricao` varchar(255) NOT NULL,
-	`palavra_chave` varchar(255) NOT NULL,
+	`adulto` BOOLEAN NOT NULL,
+	`infantil` BOOLEAN NOT NULL,
+	`acessorio` BOOLEAN NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `tb_produto` (
 	`id` bigint NOT NULL AUTO_INCREMENT,
 	`nome` varchar(255) NOT NULL,
-	`tamanho` varchar(255) NOT NULL,
-	`cor` varchar(255) NOT NULL,
-	`descricao` varchar(255) NOT NULL,
 	`preco` DECIMAL(5,2) NOT NULL,
+	`tamanho` varchar NOT NULL,
+	`cor` varchar NOT NULL,
+	`descricao` varchar NOT NULL,
 	`usuario_id` bigint NOT NULL,
 	`categoria_id` bigint NOT NULL,
 	PRIMARY KEY (`id`)
@@ -32,3 +30,7 @@ CREATE TABLE `tb_produto` (
 ALTER TABLE `tb_produto` ADD CONSTRAINT `tb_produto_fk0` FOREIGN KEY (`usuario_id`) REFERENCES `tb_usuario`(`id`);
 
 ALTER TABLE `tb_produto` ADD CONSTRAINT `tb_produto_fk1` FOREIGN KEY (`categoria_id`) REFERENCES `tb_categoria`(`id`);
+
+
+
+
