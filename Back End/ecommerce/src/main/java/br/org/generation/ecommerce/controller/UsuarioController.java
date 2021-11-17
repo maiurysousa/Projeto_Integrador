586a7baadb7459570d2bd3287c66b8fd7d47520b
glaubertorres <glauberthierryt@gmail.com>
2021-11-17 10:45:51 -0300

    adicionando usuáriocontroller e model usuarioLogin

---
+++ Back End/ecommerce/src/main/java/br/org/generation/ecommerce/controller/UsuarioController.java
@@ -0,0 +1,20 @@
+package br.org.generation.ecommerce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+	
+	
+	//@Autowired
+	//private UsuarioService usuarioService;
+	
+	//@Autowired
+	//private UsuarioRepository usuarioRepository;
+	
+}
