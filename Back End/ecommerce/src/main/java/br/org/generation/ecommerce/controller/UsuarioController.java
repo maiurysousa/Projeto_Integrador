c088c7443d2cc722d3e33cbadf564689c194d093
felipedsales <felipe_ssales@hotmail.com>
2021-11-17 13:22:35 -0300

    Adicionando as camadas de Service e Security

--- Back End/ecommerce/src/main/java/br/org/generation/ecommerce/controller/UsuarioController.java
+++ Back End/ecommerce/src/main/java/br/org/generation/ecommerce/controller/UsuarioController.java
@@ -1,9 +1,25 @@
 package br.org.generation.ecommerce.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.org.generation.ecommerce.model.Usuario;
+import br.org.generation.ecommerce.model.UsuarioLogin;
+import br.org.generation.ecommerce.repository.UsuarioRepository;
+import br.org.generation.ecommerce.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -11,10 +27,68 @@ import org.springframework.web.bind.annotation.RestController;
 public class UsuarioController {
 	
 	
-	//@Autowired
-	//private UsuarioService usuarioService;
+	/**
+	 * Faz uma injeção de dependência da classe de Serviço UsuarioService
+	 * para ter acesso aos métodos do CRUD com regras de negócio
+	 */
+	@Autowired
+	private UsuarioService usuarioService;
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	
-	//@Autowired
-	//private UsuarioRepository usuarioRepository;
+	@GetMapping("/all")
+	public ResponseEntity <List<Usuario>> getAll(){
+		
+		return ResponseEntity.ok(usuarioRepository.findAll());
+		
+	}
+	
+	/**
+	 * Executa o método autenticarUsuario da classe de serviço para efetuar
+	 * o login na api. O método da classe Controladora checa se deu certo e
+	 * exibe as mensagens (Response Status) pertinentes. 
+	 * 
+	 * Caso o login tenha sido bem sucedido, os dados do usuário e o token 
+	 * são exibidos.
+	 */
+	@PostMapping("/logar")
+	public ResponseEntity<UsuarioLogin> login(@RequestBody Optional<UsuarioLogin> user) {
+		return usuarioService.autenticarUsuario(user)
+			.map(resposta -> ResponseEntity.ok(resposta))
+			.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+	/**
+	 * Executa o método cadastrarUsuario da classe de serviço para criar
+	 * um novo usuário na api. O método da classe Controladora checa se 
+	 * deu certo e exibe as mensagens (Response Status) pertinentes. 
+	 * 
+	 * Caso cadastro tenha sido bem sucedido, os dados do usuário são 
+	 * exibidos.
+	 */
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> postUsuario(@Valid @RequestBody Usuario usuario) {
+
+		return usuarioService.cadastrarUsuario(usuario)
+			.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(resposta))
+			.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+
+	}
+
+	/**
+	 * Executa o método atualizarUsuario da classe de serviço para atualizar
+	 * os dados de um usuário na api. O método da classe Controladora checa 
+	 * se deu certo e exibe as mensagens (Response Status) pertinentes. 
+	 * 
+	 * Caso a atualização tenha sido bem sucedida, os dados do usuário 
+	 * atualizados são exibidos.
+	 */
+	@PutMapping("/atualizar")
+	public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario) {
+		return usuarioService.atualizarUsuario(usuario)
+			.map(resposta -> ResponseEntity.status(HttpStatus.OK).body(resposta))
+			.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+	}
 	
 }
